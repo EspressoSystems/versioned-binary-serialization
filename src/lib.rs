1d16770b3f44113500ec4e4472c3e864c9948988
Nathan F Yospe <nyospe@gmail.com>
2024-02-19 12:46:17 -0500

    Best not to use the same name for the type and the trait...

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ pub mod binary_serializer;
 pub mod bincode_serializer;
 pub mod version;
 
-pub type BinarySerializer<const MAJOR: u16, const MINOR: u16> =
+pub use binary_serializer::BinarySerializer;
+pub type Serializer<const MAJOR: u16, const MINOR: u16> =
     bincode_serializer::BincodeSerializer<MAJOR, MINOR>;
