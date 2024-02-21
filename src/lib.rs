5f306b4d77560f8e557aade5ea28b475be933199
Nathan F Yospe <nyospe@gmail.com>
2024-02-21 18:39:24 -0500

    Once more into the breach

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,6 @@ pub mod binary_serializer;
 pub mod bincode_serializer;
 pub mod version;
 pub mod versioned;
+
+pub use crate::binary_serializer::BinarySerializer;
+pub use crate::bincode_serializer::BincodeSerializer as Serializer;
