87c69800364979a66c55cf5518263b0a55792c3e
Nathan F Yospe <nyospe@gmail.com>
2024-02-21 18:08:02 -0500

    So... is cachix-action@v14 broken?

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,3 @@ pub mod binary_serializer;
 pub mod bincode_serializer;
 pub mod version;
 pub mod versioned;
-
-pub use binary_serializer::BinarySerializer;
-pub type Serializer<const MAJOR: u16, const MINOR: u16> =
-    bincode_serializer::BincodeSerializer<MAJOR, MINOR>;
