df9cb82543a527fef0da1e841df23cff221ba94c
nyospe <nyospe@users.noreply.github.com>
2024-02-27 00:04:52 +0000

    deploy: 2bb9b0b1a974399c13a5057c6ae89f34fb260e76

---
+++ trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"versioned_binary_serialization":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.197/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"versioned_binary_serialization/version/struct.Version.html\" title=\"struct versioned_binary_serialization::version::Version\">Version</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
