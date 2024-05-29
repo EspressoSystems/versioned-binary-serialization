6f67bb57a469cd088e885fc445769136d91e00d0
Abdul Basit <baronbasit@gmail.com>
2024-05-29 22:26:43 +0500

    clippy

--- src/binary_serializer.rs
+++ src/binary_serializer.rs
@@ -15,17 +15,13 @@ pub trait BinarySerializer {
 
     // TODO: `Versioned` trait
 
-    fn serialize_no_version<T: ?Sized>(value: &T) -> Result<Vec<u8>>
-    where
-        T: Serialize;
+    fn serialize_no_version<T: ?Sized + Serialize>(value: &T) -> Result<Vec<u8>>;
 
     fn deserialize_no_version<'a, T>(bytes: &'a [u8]) -> Result<T>
     where
         T: Deserialize<'a>;
 
-    fn serialize<T: ?Sized>(value: &T) -> Result<Vec<u8>>
-    where
-        T: Serialize;
+    fn serialize<T: ?Sized + Serialize>(value: &T) -> Result<Vec<u8>>;
 
     fn deserialize<'a, T>(bytes: &'a [u8]) -> Result<T>
     where
